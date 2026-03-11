2a7c6aae2cc9e0b217035536a60ac634ad6dde28
Thiago Pires <thiagopac@gmail.com>
2026-03-11 12:05:44 -0300

    Initial Commit

---
+++ SwiftAtlas/SwiftAtlasApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftAtlasApp.swift
+//  SwiftAtlas
+//
+//  Created by Thiago Castro on 11/03/26.
+//
+
+
+import SwiftUI
+
+@main
+struct SwiftAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftAtlasTests/SwiftAtlasTests.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftAtlasTests.swift
+//  SwiftAtlas
+//
+//  Created by Thiago Castro on 11/03/26.
+//
+
+
+import Testing
+@testable import SwiftAtlas
+
+struct SwiftAtlasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
